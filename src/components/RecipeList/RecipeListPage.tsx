f3fa63eb82bb62f413d134b6a1a9c357c060e6c0
shota <30305829kasukabe@gmail.com>
2024-09-20 19:18:15 +0900

    RecipeCard完成

--- src/components/RecipeList/RecipeListPage.tsx
+++ src/components/RecipeList/RecipeListPage.tsx
@@ -1,10 +1,108 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { CoffeeRecipeCard } from './CoffeeRecipeCard';
+
+interface RecipeStep {
+    time: string;
+    action: string;
+}
+
+interface CoffeeRecipe {
+    id: string;
+    recipeName: string;
+    description?: string;
+    beanName?: string;
+    beanWeight: string;
+    roastLevel: string;
+    grindSize: string;
+    totalWaterAmount: string;
+    totalTime: string;
+    steps: RecipeStep[];
+    isLiked: boolean;
+    isBookmarked: boolean;
+}
 
 export const RecipeListPage = () => {
+    const recipeList: CoffeeRecipe[] = [
+        {
+            id: '1',
+            recipeName: 'Pour Over Coffee',
+            description: 'A clean and flavorful brew method',
+            beanName: 'Kenya',
+            beanWeight: '15g',
+            grindSize: 'Medium grind',
+            roastLevel: 'Dark',
+            totalWaterAmount: '360g f',
+            totalTime: '3:30',
+            steps: [
+                { time: '0:00', action: 'Add 60 g of hot water' },
+                { time: '0:45', action: 'Add 60 g of hot water' },
+                { time: '1:30', action: 'Add 60 g of hot water' },
+                { time: '2:15', action: 'Add 60 g of hot water' },
+                { time: '2:45', action: 'Add 60 g of hot water' },
+                { time: '3:30', action: 'Remove dripper' },
+            ],
+            isLiked: true,
+            isBookmarked: true,
+        },
+        {
+            id: '2',
+            recipeName: 'Pour Over Coffee',
+            description: 'A clean and flavorful brew method',
+            beanName: 'Kenya',
+            beanWeight: '15g',
+            grindSize: 'Medium grind',
+            roastLevel: 'Dark',
+            totalWaterAmount: '360g',
+            totalTime: '3:30',
+            steps: [
+                { time: '0:00', action: 'Add 60 g of hot water' },
+                { time: '0:45', action: 'Add 60 g of hot water' },
+                { time: '1:30', action: 'Add 60 g of hot water' },
+                { time: '2:15', action: 'Add 60 g of hot water' },
+                { time: '2:45', action: 'Add 60 g of hot water' },
+                { time: '3:30', action: 'Remove dripper' },
+            ],
+            isLiked: false,
+            isBookmarked: false,
+        },
+    ];
+
+    const [recipes, setRecipes] = useState<CoffeeRecipe[]>(recipeList);
+
+    const handleLikeToggle = (id: string) => {
+        setRecipes((prevRecipeList) =>
+            prevRecipeList.map((recipe) =>
+                recipe.id === id
+                    ? { ...recipe, isLiked: !recipe.isLiked }
+                    : recipe,
+            ),
+        );
+    };
+
+    const handleBookmarkToggle = (id: string) => {
+        setRecipes((prevRecipeList) =>
+            prevRecipeList.map((recipe) =>
+                recipe.id === id
+                    ? { ...recipe, isBookmarked: !recipe.isBookmarked }
+                    : recipe,
+            ),
+        );
+    };
+
     return (
         <div className="container mx-auto">
-            <h2>Recipe Listページです</h2>
-            <Link to="/">Homeページへ</Link>
+            <h2>Recipe Listページ</h2>
+            <div className="flex flex-col gap-3">
+                {recipes.map((recipe) => (
+                    <CoffeeRecipeCard
+                        key={recipe.id}
+                        {...recipe}
+                        onLikeToggle={() => handleLikeToggle(recipe.id)}
+                        onBookmarkToggle={() => handleBookmarkToggle(recipe.id)}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,11 +3,11 @@ import coffeeIcon from '@/assets/coffee_icon.svg';
 
 export const Header = () => {
     return (
-        <header className="fixed top-0 left-0 right-0 bg-gray-300 z-10">
+        <header className="fixed left-0 right-0 top-0 z-10 bg-gray-300">
             <div className="container mx-auto px-4">
-                <nav className="flex justify-between items-center py-4">
+                <nav className="flex items-center justify-between py-4">
                     <NavLink
-                        className="flex text-2xl font-bold space-x-2 items-center"
+                        className="flex items-center space-x-2 text-2xl font-bold"
                         to="/"
                     >
                         <img src={coffeeIcon} />
@@ -16,7 +16,7 @@ export const Header = () => {
                     <ul className="flex space-x-5">
                         <li>
                             <NavLink
-                                className="hover:text-green-900 transition-all duration-300"
+                                className="transition-all duration-300 hover:text-green-900"
                                 to="/recipes"
                             >
                                 Recipe List
@@ -24,7 +24,7 @@ export const Header = () => {
                         </li>
                         <li>
                             <NavLink
-                                className="hover:text-green-900 transition-all duration-300"
+                                className="transition-all duration-300 hover:text-green-900"
                                 to="/mypage"
                             >
                                 My Page
