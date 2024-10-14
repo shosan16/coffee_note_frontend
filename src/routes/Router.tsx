852a7474f2cae4fedb1eba1080d3f7f22a70d096
shota <30305829kasukabe@gmail.com>
2024-10-14 12:58:24 +0900

    404ページの作成

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,6 +4,7 @@ import { AllRecipesPage } from '@/features/all-recipes/AllRecipesPage';
 import { MyPage } from '@/features/my-page/MyPage';
 import { GroupPage } from '@/features/group/GroupPage';
 import { GroupRecipesPage } from '@/features/group-recipes/GroupRecipesPage';
+import { NotFoundPage } from '@/features/error/NotFoundPage';
 
 export const Router = () => {
     return (
@@ -13,6 +14,7 @@ export const Router = () => {
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/groups" element={<GroupPage />} />
             <Route path="/groups/:group_id" element={<GroupRecipesPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
 };
